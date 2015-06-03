5022cf5b673378754cb7da33e970cbfbbea15030
Eric Foard <foard265@gmail.com>
2015-06-02 18:04:06 -0700

    commit to switch branch

---
+++ scrawl/src/AppBundle/Entity/Tag.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tag
+ *
+ * @ORM\Table(name="scrawl_tags")
+ * @ORM\Entity
+ */
+class Tag
+{
+	 /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+	 private $id;
+
+	 /**
+     * @ORM\Column(type="string", length=60, unique=true)
+     */
+	 private $tagName;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set tagName
+     *
+     * @param string $tagName
+     * @return Tag
+     */
+    public function setTagName($tagName)
+    {
+        $this->tagName = $tagName;
+
+        return $this;
+    }
+
+    /**
+     * Get tagName
+     *
+     * @return string 
+     */
+    public function getTagName()
+    {
+        return $this->tagName;
+    }
+}
