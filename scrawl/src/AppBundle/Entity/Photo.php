f9bcbb9b2e19d9c454a30688e400e1e473eac6fa
Eric Foard <foard265@gmail.com>
2015-06-04 18:32:10 -0700

    updated gitignore to ignore uploaded photos, improvements to photo upload behavior. now photos display

--- scrawl/src/AppBundle/Entity/Photo.php
+++ scrawl/src/AppBundle/Entity/Photo.php
@@ -204,7 +204,7 @@ class Photo
     {
         // the absolute directory path where uploaded
         // documents should be saved
-        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+        return __DIR__.'/../../../web/'.$this->getUploadDir();
     }
 
     protected function getUploadDir()
