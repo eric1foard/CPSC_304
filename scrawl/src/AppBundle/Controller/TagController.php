0702d45f0a2ed74e2a3f0fbc064082326d6ef8eb
Yi-ping Benjamin Liu <yipingbenjamin.l@gmail.com>
2015-06-14 15:28:45 -0700

    rewrote tagcontrolled with SQL(unverified), edited user.edit.html.twig for logical check of admin user

--- scrawl/src/AppBundle/Controller/TagController.php
+++ scrawl/src/AppBundle/Controller/TagController.php
@@ -21,9 +21,20 @@ class TagController extends Controller
      */
     public function indexAction()
     {
-        $em = $this->getDoctrine()->getManager();
+        //$em = $this->getDoctrine()->getManager();
 
-        $entities = $em->getRepository('AppBundle:Tag')->findAll();
+        $sql = 'SELECT * FROM scrawl_tags';
+
+        $stmt = $this->getDoctrine()->getManager()->getConnection()->prepare($sql);
+
+        //execute query
+        $stmt->execute();
+
+        //get all rows of results 
+        $entities = $stmt->fetchAll();
+
+        //original generated code
+        //$entities = $em->getRepository('AppBundle:Tag')->findAll();
 
         return $this->render('AppBundle:Tag:index.html.twig', array(
             'entities' => $entities,
@@ -40,13 +51,24 @@ class TagController extends Controller
         $form->handleRequest($request);
 
         if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
+            //original generated code
+            //$em = $this->getDoctrine()->getManager();
             $em->persist($entity);
             $em->flush();
 
+            $sql = 'INSERT INTO scrawl_tags value(:id, :tagName)';
+            $stmt = $this->getDoctrine()->getManager()->getConnection()->prepare($sql);
+
+            $stmt->bindValue('id', 104);
+            $stmt->bindValue('tagName', $form["tags"]->getData());
+
+            $stmt->execute();            
+
+            $this->get('session')->getFlashBag()->add('notice','tag successfully added');
             return $this->redirect($this->generateUrl('tag_show', array('id' => $entity->getId())));
         }
 
+        $this->get('session')->getFlashBag()->add('notice','tag addition error, please try again');
         return $this->render('AppBundle:Tag:new.html.twig', array(
             'entity' => $entity,
             'form'   => $form->createView(),
@@ -93,9 +115,22 @@ class TagController extends Controller
      */
     public function showAction($id)
     {
-        $em = $this->getDoctrine()->getManager();
+        //original generated code
+        //$em = $this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('AppBundle:Tag')->find($id);
+        //original generated code
+        //$entity = $em->getRepository('AppBundle:Tag')->find($id);
+
+        $sql = 'SELECT * FROM scrawl_tags WHERE id=?';
+        $stmt = $this->getDoctrine()->getManager()->getConnection()->prepare($sql);
+
+        $stmt->bindValue(1, $id);
+
+        $stmt->execute();
+
+        $entity = $stmt->fetch();
+
+        
 
         if (!$entity) {
             throw $this->createNotFoundException('Unable to find Tag entity.');
@@ -115,9 +150,28 @@ class TagController extends Controller
      */
     public function editAction($id)
     {
-        $em = $this->getDoctrine()->getManager();
+        //original generated code
+        //$em = $this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('AppBundle:Tag')->find($id);
+        //original generated code
+        //$entity = $em->getRepository('AppBundle:Tag')->find($id);
+
+        $sql = 'SELECT * FROM scrawl_tags WHERE id=?';
+
+        $stmt = $this->getDoctrine()->getManager()->getConnection()->prepare($sql);
+
+        $stmt->bindValue(1, $id);
+        $stmt->execute();
+
+        $result = $stmt->fetch();
+
+        $entity = new Tag();
+        $editForm = $this->createEditForm($entity, $id);
+
+        $editForm->get('id')->setData($result['id']);
+        $editForm->get('tagName')->setData($result['tagName']);
+
+
 
         if (!$entity) {
             throw $this->createNotFoundException('Unable to find Tag entity.');
@@ -128,8 +182,9 @@ class TagController extends Controller
 
         return $this->render('AppBundle:Tag:edit.html.twig', array(
             'entity'      => $entity,
+            'id'          => $id,
             'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+            //'delete_form' => $deleteForm->createView(),
         ));
     }
 
@@ -157,29 +212,58 @@ class TagController extends Controller
      */
     public function updateAction(Request $request, $id)
     {
-        $em = $this->getDoctrine()->getManager();
+        //original generated code
+        //$em = $this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('AppBundle:Tag')->find($id);
+        //original generated code
+        //$entity = $em->getRepository('AppBundle:Tag')->find($id);
 
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Tag entity.');
+        //original generated code
+        //if (!$entity) {
+        //    throw $this->createNotFoundException('Unable to find Tag entity.'); }
+
+        //$deleteForm = $this->createDeleteForm($id);
+        //$editForm = $this->createEditForm($entity);
+        //$editForm->handleRequest($request);
+
+        //if ($editForm->isValid()) {
+        //    $em->flush();
+
+        try
+        {
+            $postParamsHash = $request->request->get('appbundle_tag', 'does not exist!');
+
+            $tags = $postParamsHash['tag'];
+
+            $sql = 'UPDATE scrawl_photos SET longitude=:tags WHERE id=:id';
+
+            $stmt = $this->getDoctrine()->getManager()
+            ->getConnection()->prepare($sql);
+
+     //bind variables
+            $stmt->bindValue('id', $id);
+            $stmt->bindValue('tags', $tags);
+
+     //execute query
+            $stmt->execute();
+        }
+        catch (\Doctrine\DBAL\DBALException $e) {
+
+            $this->get('session')->getFlashBag()
+            ->add('error','there was a problem updating the tag! Please try again.');
+
+            return $this->redirect($this->generateUrl('homepage'));
         }
 
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createEditForm($entity);
-        $editForm->handleRequest($request);
 
-        if ($editForm->isValid()) {
-            $em->flush();
+        $this->get('session')->getFlashBag()->add('notice', 'tag successfully updated')
+        return $this->redirect($this->generateUrl('tag_edit', array('id' => $id)));
+        //}
 
-            return $this->redirect($this->generateUrl('tag_edit', array('id' => $id)));
-        }
-
-        return $this->render('AppBundle:Tag:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
+        //return $this->render('AppBundle:Tag:edit.html.twig', array(
+        //    'entity'      => $entity,
+        //    'edit_form'   => $editForm->createView(),
+        //    'delete_form' => $deleteForm->createView(),));
     }
     /**
      * Deletes a Tag entity.
@@ -187,38 +271,29 @@ class TagController extends Controller
      */
     public function deleteAction(Request $request, $id)
     {
-        $form = $this->createDeleteForm($id);
-        $form->handleRequest($request);
+        //$form = $this->createDeleteForm($id);
+        //$form->handleRequest($request);
 
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('AppBundle:Tag')->find($id);
+        //if ($form->isValid()) {
+        //    $em = $this->getDoctrine()->getManager();
+        //    $entity = $em->getRepository('AppBundle:Tag')->find($id);
 
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find Tag entity.');
-            }
+        //    if (!$entity) {
+        //        throw $this->createNotFoundException('Unable to find Tag entity.');}
 
-            $em->remove($entity);
-            $em->flush();
-        }
+        //    $em->remove($entity);
+        //    $em->flush();}
+
+        $sql = 'DELETE FROM scrawl_tags WHERE id=?';
+
+        $stmt = $this->getDoctrine()->getManager()->getConnection()->prepare($sql);
+
+        //replace ? in query with $id
+        $stmt->bindValue(1, $id);
+
+        //execute query
+        $stmt->execute();
 
         return $this->redirect($this->generateUrl('tag'));
-    }
-
-    /**
-     * Creates a form to delete a Tag entity by id.
-     *
-     * @param mixed $id The entity id
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('tag_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
-            ->getForm()
-        ;
     }
 }
