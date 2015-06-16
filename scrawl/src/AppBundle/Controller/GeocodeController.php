69ca89d55c4660aadad16cfd6991a54a8b1a9110
Eric Foard <foard265@gmail.com>
2015-06-15 19:26:21 -0700

    removed references to ID from photo ctrl index, initialized empty class for geocode ctrl and defined it as a service

---
+++ scrawl/src/AppBundle/Controller/GeocodeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Config\Definition\Exception\Exception;
+
+/**
+ * Photo controller.
+ *
+ */
+class GeocodeController extends Controller
+{
+
+
+}
+
